dbbb3e2385857b3fa838c799dea55594e71c57e1
parksomii <somi020605@gmail.com>
2025-10-31 22:54:14 +0900

    feat: 로또 게임 규칙 상수 클래스 추가

---
+++ src/main/java/lotto/model/constant/LottoRules.java
@@ -0,0 +1,10 @@
+package lotto.model.constant;
+
+public final class LottoRules {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
+    private LottoRules() { }
+}
