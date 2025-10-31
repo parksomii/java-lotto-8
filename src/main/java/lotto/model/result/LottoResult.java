ddd8e72296f1ccc0532df525211f3a129ab4e92d
parksomii <somi020605@gmail.com>
2025-11-01 00:17:45 +0900

    feat: 로또 당첨 결과 집계 및 총 상금 계산 기능 추가

---
+++ src/main/java/lotto/model/result/LottoResult.java
@@ -0,0 +1,30 @@
+package lotto.model.result;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<LottoRank, Integer> rankCounts = new EnumMap<>(LottoRank.class);
+
+    public LottoResult() {
+        for (LottoRank rank : LottoRank.values()) {
+            rankCounts.put(rank, 0);
+        }
+    }
+
+    public void add(LottoRank rank) {
+        rankCounts.put(rank, rankCounts.get(rank) + 1);
+    }
+
+    public int getCount(LottoRank rank) {
+        return rankCounts.get(rank);
+    }
+
+    public long totalPrize() {
+        long sum = 0L;
+        for (Map.Entry<LottoRank, Integer> e : rankCounts.entrySet()) {
+            sum += e.getKey().getPrize() * e.getValue();
+        }
+        return sum;
+    }
+}
