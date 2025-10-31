ed605370c31f3d5898b613936da995428a0937e6
parksomii <somi020605@gmail.com>
2025-10-31 23:10:40 +0900

    feat: 구입 금액(Budget) 모델 및 티켓 수 계산 기능 추가

---
+++ src/main/java/lotto/model/lotto/Budget.java
@@ -0,0 +1,17 @@
+package lotto.model.lotto;
+
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
+import lotto.model.constant.LottoRules;
+
+public record Budget(int amount) {
+    public Budget {
+        if (amount <= 0 || amount % LottoRules.LOTTO_PRICE != 0) {
+            throw new LottoException(ErrorMessage.BUDGET_UNIT);
+        }
+    }
+
+    public int ticketCount() {
+        return amount / LottoRules.LOTTO_PRICE;
+    }
+}
