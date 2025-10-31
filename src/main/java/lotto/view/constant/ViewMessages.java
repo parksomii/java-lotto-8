8c0ff32d0005eb335179f0d1533ef2b3f1e88a66
parksomii <somi020605@gmail.com>
2025-11-01 00:04:27 +0900

    feat: 출력 메시지 상수(ViewMessages) 추가

---
+++ src/main/java/lotto/view/constant/ViewMessages.java
@@ -0,0 +1,21 @@
+package lotto.view.constant;
+
+public final class ViewMessages {
+    public static final String PURCHASE_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String WINNING_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_PROMPT = "보너스 번호를 입력해 주세요.";
+
+    public static final String PURCHASED_COUNT_SUFFIX = "개를 구매했습니다.";
+
+    public static final String STATS_TITLE = "당첨 통계";
+    public static final String STATS_DIVIDER = "---";
+
+    public static final String COUNT_UNIT = "개";
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+    public static final String HYPHEN_WITH_SPACES = " - ";
+
+    public static final String YIELD_PREFIX = "총 수익률은 ";
+    public static final String YIELD_SUFFIX = "%입니다.";
+
+    private ViewMessages() { }
+}
