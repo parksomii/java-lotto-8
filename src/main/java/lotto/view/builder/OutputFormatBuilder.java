b710e077980634eecaf104c92ab9637c18ae4e67
parksomii <somi020605@gmail.com>
2025-11-01 00:10:11 +0900

    feat: 출력 문자열 포맷팅 기능 추가

---
+++ src/main/java/lotto/view/builder/OutputFormatBuilder.java
@@ -0,0 +1,41 @@
+package lotto.view.builder;
+
+import lotto.model.lotto.Lotto;
+import lotto.model.result.LottoRank;
+import lotto.model.result.LottoResult;
+import lotto.view.constant.ViewMessages;
+
+import java.util.List;
+
+public final class OutputFormatBuilder {
+    private OutputFormatBuilder() {
+    }
+
+    public static String tickets(List<Lotto> tickets) {
+        StringBuilder sb = new StringBuilder();
+        for (Lotto lotto : tickets) {
+            sb.append(lotto.getNumbers().toString()).append(ViewMessages.LINE_SEPARATOR);
+        }
+        return sb.toString();
+    }
+
+    public static String statistics(LottoResult result) {
+        return String.join(ViewMessages.LINE_SEPARATOR,
+                ViewMessages.STATS_TITLE,
+                ViewMessages.STATS_DIVIDER,
+                line(result, LottoRank.FIFTH),
+                line(result, LottoRank.FOURTH),
+                line(result, LottoRank.THIRD),
+                line(result, LottoRank.SECOND),
+                line(result, LottoRank.FIRST),
+                ""
+        );
+    }
+
+    private static String line(LottoResult result, LottoRank rank) {
+        return rank.getDescription()
+                + ViewMessages.HYPHEN_WITH_SPACES
+                + result.getCount(rank)
+                + ViewMessages.COUNT_UNIT;
+    }
+}
